e3332873d6fd15b788248d7e023730fbc1a0866c
Ipsita Mohapatra <Ipsita Mohapatra@localhost>
2013-09-23 12:10:21 +0800

    Test commit from Ipsita

--- src/sg/edu/nus/cs2103/sudo/COMMAND_TYPE.java
+++ src/sg/edu/nus/cs2103/sudo/COMMAND_TYPE.java
@@ -3,7 +3,7 @@ package sg.edu.nus.cs2103.sudo;
 //Test from yos
 // Don't worry about the syntax error below.
 
-// ipsita says hi
+// ipsita says hi again
 public enum COMMAND_TYPE {  
     ADD, DELETE, EDIT, UNDO, REDO, SEARCH, SORT, HELP
 }

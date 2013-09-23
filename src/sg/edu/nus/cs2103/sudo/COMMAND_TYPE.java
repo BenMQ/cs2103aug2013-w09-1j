59cfec76328a8e9c3800048eba21c3764519f77c
Ipsita Mohapatra <ipsitamohapatra93@gmail.com>
2013-09-23 11:21:13 +0800

    added a random comment to say hi

--- src/sg/edu/nus/cs2103/sudo/COMMAND_TYPE.java
+++ src/sg/edu/nus/cs2103/sudo/COMMAND_TYPE.java
@@ -2,6 +2,7 @@ package sg.edu.nus.cs2103.sudo;
 
 // Don't worry about the syntax error below.
 
+// ipsita says hi
 public enum COMMAND_TYPE {  
     ADD, DELETE, EDIT, UNDO, REDO, SEARCH, SORT, HELP
 }

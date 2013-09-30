f3cb9a475466d14295fe746c7a657671d92c3245
Minqi Chen <cnchenminqi@gmail.com>
2013-09-30 21:18:32 +0800

    Added TaskManager with basic add and print function

---
+++ src/sg/edu/nus/cs2103/sudo/logic/TaskManager.java
@@ -0,0 +1,65 @@
+package sg.edu.nus.cs2103.sudo.logic;
+
+import java.util.ArrayList;
+
+/**
+ * 
+ * @author chenminqi
+ *
+ */
+public class TaskManager {
+    
+    // A list of non-floating tasks
+    private ArrayList<Task> normalTasks;
+    
+    public TaskManager() {
+        normalTasks = new ArrayList<Task>();
+    }
+    
+    /**
+     * Load an ArrayList of tasks into memory.
+     * This method should be called upon launch after the storage unit
+     * has read the stored item from disk. This action overrides any
+     * exiting tasks stored in memory
+     * 
+     * @param tasks ArrayList of tasks that is provided by the storage unit
+     */
+    public void preloadTasks(ArrayList<Task> tasks) {
+        normalTasks = tasks;
+    }
+    
+    /**
+     * Adds a new constructed task.
+     * @param newTask
+     */
+    public void addTask(Task newTask) {
+        // the new task should be inserted such that the memory list is 
+        // maintained as a sorted list
+        normalTasks.add(newTask);
+    }
+    
+    /**
+     * Prints tasks to stdout. Not completed tasks are always printed.
+     * If showAll is set to true, completed tasks are printed as well.
+     * @param showAll set to true to include completed task
+     */
+    public void displayAllTasks(boolean showAll) {
+        int serial = 0;
+        for (int i = 0; i < normalTasks.size(); i ++) {
+            Task task = normalTasks.get(i);
+            if (showAll || !task.isComplete) {
+                serial++;
+                System.out.println(serial + task.toString());
+            }
+        }
+    }
+    
+    /**
+     * Prints all tasks that are not yet completed
+     */
+    public void displayAllTasks() {
+        displayAllTasks(false);
+    }
+    
+    
+}

ddb5ed1875257de0ad495e99c7cb66258d7b0dc7
Minqi Chen <cnchenminqi@gmail.com>
2013-10-02 09:13:59 +0800

    Replace NotImplementedException with UnsupportedOperationException

---
+++ src/sg/edu/nus/cs2103/sudo/logic/FloatingTask.java
@@ -0,0 +1,25 @@
+package sg.edu.nus.cs2103.sudo.logic;
+
+/**
+ * @author Ipsita 
+ *
+ */
+public class FloatingTask extends Task {
+	
+	// Floating object constructor
+	public FloatingTask() {
+	}
+	
+	// Floating object constructor
+	public FloatingTask(String description) {
+		editDescription(description);
+		numOfTasks++;
+		editId(numOfTasks);
+	}
+	
+	// To be used to store in .txt 
+	public String toStringForFile() {
+		return description + " " + isComplete + "\n";
+	}
+
+}
---
+++ src/sg/edu/nus/cs2103/sudo/logic/Task.java
@@ -0,0 +1,102 @@
+package sg.edu.nus.cs2103.sudo.logic;
+
+import org.joda.time.DateTime;
+
+/**
+ * @author Ipsita 
+ *
+ */
+
+public abstract class Task implements Comparable<Task>{
+	protected int id;
+	protected String description = "";
+	protected Boolean isComplete = false;
+	protected DateTime startTime;
+	protected DateTime endTime;
+	protected static int numOfTasks = 0;
+	
+	// Task object constructor
+	public Task() {
+	}
+	
+	public Task(int id, String desc, DateTime startTime, DateTime endTime) {
+	    this.id = id;
+	    this.description = desc;
+	    this.startTime = startTime;
+	    this.endTime = endTime;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public boolean getComplete() {
+		return isComplete;
+	}
+	
+	/**
+	 * Converts the task object into a string with the following format:
+	 * 1) Timed task: desc from x to y
+	 * 2) Deadline task: desc by x
+	 * 3) Floating task: desc
+	 */
+	public String toString() {
+	    String output = description;
+	    if (startTime != null) {
+	        output += " from " + startTime.toString() + " to " + endTime.toString();
+	    } else if (endTime != null) {
+	        output += " by " + endTime.toString();
+	    }
+	    
+	    return output;
+	}
+	
+	public void editId(int id) {
+		this.id = id;
+	}
+	
+	public void editDescription(String newDescription) {
+		description = newDescription;
+	}
+	
+	public void editStartTime(DateTime startTime) {
+		this.startTime = startTime;
+	}
+	
+	public void editEndTime(DateTime endTime) {
+		this.endTime = endTime;
+	}
+	
+	public void markCompleted() {
+		this.isComplete = true;
+	}
+	
+	public void markNotCompleted() {
+	    this.isComplete = false;
+	}
+	
+	public void toggleCompleted() {
+	    this.isComplete = !this.isComplete;
+	}
+	
+	/**
+	 * Implements comparable interface. Tasks are sorted by their end time.
+	 * Floating tasks are always at the end, but they are managed separately anyway.
+	 */
+	public int compareTo(Task x) {
+        if (this.endTime != null) {
+            if (x.endTime != null) {
+                return this.endTime.compareTo(x.endTime);
+            } else {
+                return -1;
+            }
+        } else {
+            if (x.endTime != null) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+	}
+	
+}

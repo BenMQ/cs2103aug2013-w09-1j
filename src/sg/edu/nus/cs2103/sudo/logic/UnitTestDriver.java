2585f97fa0b25a6cbe05fb6e5a02c55cc9585744
Yos Riady <yosriady@gmail.com>
2013-10-09 17:42:40 +0800

    Add UnitTestDriver

---
+++ src/sg/edu/nus/cs2103/sudo/logic/UnitTestDriver.java
@@ -0,0 +1,11 @@
+package sg.edu.nus.cs2103.sudo.logic;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ InputParserTest.class, TaskManagerTest.class })
+public class UnitTestDriver {
+
+}

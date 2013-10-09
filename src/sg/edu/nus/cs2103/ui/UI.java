ef539c15f8626a36068cf9f30d8789f62b42cecd
Yos Riady <yosriady@gmail.com>
2013-10-10 02:03:52 +0800

    Refactor InputParser constructor to increase testability

--- src/sg/edu/nus/cs2103/ui/UI.java
+++ src/sg/edu/nus/cs2103/ui/UI.java
@@ -3,13 +3,14 @@ package sg.edu.nus.cs2103.ui;
 import java.util.Scanner;
 
 import sg.edu.nus.cs2103.sudo.logic.InputParser;
+import sg.edu.nus.cs2103.sudo.logic.TaskManager;
 
 public class UI {
 	
 	public static void main(String[] args) {
 		
 		Scanner user = new Scanner( System.in );
-		InputParser parser = new InputParser();
+		InputParser parser = new InputParser(new TaskManager());
 		while (true) {
 			String userInput = parser.readCommand(user); //listen to scanner
 			parser.executeCommand(userInput);

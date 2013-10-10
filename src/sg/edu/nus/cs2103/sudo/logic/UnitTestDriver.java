c426f806a877119aaa6ea97989f21bc2ecd6a252
Yos Riady <yosriady@gmail.com>
2013-10-10 14:05:20 +0800

    Add validations to InputParser

--- src/sg/edu/nus/cs2103/sudo/logic/UnitTestDriver.java
+++ src/sg/edu/nus/cs2103/sudo/logic/UnitTestDriver.java
@@ -4,8 +4,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import sg.edu.nus.cs2103.sudo.storage.StorageHandlerTest;
+
 @RunWith(Suite.class)
-@SuiteClasses({ InputParserTest.class, TaskManagerTest.class })
+@SuiteClasses({ InputParserTest.class, TaskManagerTest.class, StorageHandlerTest.class })
 public class UnitTestDriver {
 
 }

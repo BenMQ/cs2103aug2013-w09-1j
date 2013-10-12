018781975ef20582b8091f63bc6ba08f754a61e7
lolitacon2012 <lolitacon2012@localhost>
2013-10-12 22:51:28 +0800

    Storage can work individually with XMLSerializer now.

--- src/sg/edu/nus/cs2103/sudo/storage/XMLSerializer.java
+++ src/sg/edu/nus/cs2103/sudo/storage/XMLSerializer.java
@@ -3,22 +3,23 @@ package sg.edu.nus.cs2103.sudo.storage;
 import java.beans.XMLEncoder;
 import java.beans.XMLDecoder;
 import java.io.*;
+import java.util.ArrayList;
 
 public class XMLSerializer {
-    public static void write(History h) throws Exception{
+    public static void write(ArrayList<ArrayList<String>> h, String name) throws Exception{
         XMLEncoder encoder =
            new XMLEncoder(
               new BufferedOutputStream(
-                new FileOutputStream(h.getName())));
+                new FileOutputStream(name)));
         encoder.writeObject(h);
         encoder.close();
     }
 
-    public static History read(String filename) throws Exception {
+    public static ArrayList<ArrayList<String>> read(String filename) throws Exception {
         XMLDecoder decoder =
             new XMLDecoder(new BufferedInputStream(
                 new FileInputStream(filename)));
-        History h = (History)decoder.readObject();
+        ArrayList<ArrayList<String>> h = (ArrayList<ArrayList<String>>) decoder.readObject();
         decoder.close();
         return h;
     }

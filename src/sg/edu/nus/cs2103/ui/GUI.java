850f58d4ecccc1e81aa08f1bb000546420917930
lolitacon2012 <lolitacon2012@localhost>
2013-10-28 10:54:41 +0800

    color change on GUI

--- src/sg/edu/nus/cs2103/ui/GUI.java
+++ src/sg/edu/nus/cs2103/ui/GUI.java
@@ -115,6 +115,13 @@ public class GUI extends javax.swing.JFrame implements NativeKeyListener {
         sudooleButton = new javax.swing.JButton();
         floatingTaskScrollPane = new javax.swing.JScrollPane();
         floatingList = new javax.swing.JList();
+        
+        floatingList.setForeground(new java.awt.Color(0, 100, 245));
+        floatingList.setBackground(new java.awt.Color(150, 245, 100));
+        
+        
+        
+        
         labelFloatingTasks = new javax.swing.JLabel();
         progressBar = new javax.swing.JProgressBar();
         complecationRate = new javax.swing.JLabel();
@@ -124,8 +131,9 @@ public class GUI extends javax.swing.JFrame implements NativeKeyListener {
 
         //Initialization of properties
         mainTextFrame.setEditable(false);
-        mainTextFrame.setFont(new java.awt.Font("Monaco", 0, 13));
+        mainTextFrame.setFont(new java.awt.Font("Monaco", 0, 17));
         mainTextFrame.setForeground(new java.awt.Color(92, 100, 245));
+        mainTextFrame.setBackground(new java.awt.Color(200, 245, 100));
         mainTextFrame.setColumns(20);
         mainTextFrame.setRows(5);
         mainTextFrameScrollPane.setViewportView(mainTextFrame);

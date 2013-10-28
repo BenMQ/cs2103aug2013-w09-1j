57bb544ecc1c1d637ca6700bbee0b47cec7f6230
Ipsita Mohapatra <ipsitamohapatra93@gmail.com>
2013-10-28 11:11:14 +0800

    removed unwanted method

---
+++ src/sg/edu/nus/cs2103/sudo/Constants.java
@@ -0,0 +1,92 @@
+package sg.edu.nus.cs2103.sudo;
+
+import java.util.HashMap;
+
+public class Constants {
+	
+	public static final String MESSAGE_WELCOME_TO_SUDO_FIRST = "Welcome to the world of Sudo!\nTake some time to get yourself familiar with the most advancd personal todo-list app ever.";
+	public static final String MESSAGE_WELCOME_TO_SUDO_RELOAD = "Welcome back, dear user.\nYour past records have been loaded successfully.";
+	
+
+	public static final String MESSAGE_INVALID_COMMAND = "Invalid command\n";
+	public static final String MESSAGE_INVALID_TASK_INDEX = "Invalid task index\n";
+	public static final String MESSAGE_INCOMPLETE_COMMAND = "Incomplete command\n";	
+	public static final String MESSAGE_INVALID_SEARCH = "Search is invalid.\n";
+	public static final String MESSAGE_INVALID_DELETE = "Search term for delete is invalid.\n";
+	
+	public static final String MESSAGE_DISPLAY = "Displaying incomplete tasks\n";
+	public static final String MESSAGE_DISPLAY_ALL = "Displaying all tasks\n";
+	public static final String MESSAGE_INVALID_NUMBER_OF_DATES = "Invalid number of dates.\n";
+	public static final String MESSAGE_ADD_TIMED = "Add timed task: %s\n";
+	public static final String MESSAGE_ADD_DEADLINE = "Add deadline task: %s\n";
+	public static final String MESSAGE_ADD_FLOATING = "Add floating task: %s\n";
+	public static final String MESSAGE_SEARCH = "Searching: %s\n";
+	public static final String MESSAGE_EDIT = "Editing: %s\n";
+	public static final String MESSAGE_DELETE = "Deleted: %s\n";
+	public static final String MESSAGE_ENTER_TASK_ID = "Please enter task id:";
+	public static final String MESSAGE_MISSING_DESCRIPTION = "Task description should be between single quotes.\n";
+	
+	public static final String MESSAGE_SAME_START_END_TIME = "Start date and end date for the task are the same!\n";
+	public static final String MESSAGE_END_BEFORE_START_TIME = "End date of the task occurs before start date!\n";
+			
+	public static final String MESSAGE_EMPTY_LIST = "Task List is empty!\n";
+	public static final String MESSAGE_ALREADY_COMPLETE = "The task specified is complete.\n";
+	public static final String MESSAGE_ALREADY_INCOMPLETE = "The task specified is incomplete.\n";
+	public static final String MESSAGE_NO_SEARCH_RESULTS = "No search results found.\n";
+	public static final String MESSAGE_NO_FLOATING_TASKS = "No floating tasks.\n";
+	public static final String MESSAGE_FINISH = "Finishing task.\n";
+	public static final String MESSAGE_UNFINISH = "Un-Finishing task.\n";
+
+    public static final String MESSAGE_NO_FREE_SLOTS = "No free slots found. :(";
+    public static final String MESSAGE_FREE_SLOTS_PREFIX = "Here are your free time slots for today:";
+
+    public static final int FREE_SLOT_MINIMUM_DURATION_IN_MINUTES = 10;
+    public static final int FREE_SLOT_MINIMUM_DURATION = FREE_SLOT_MINIMUM_DURATION_IN_MINUTES * 60 * 1000; // milliseconds
+	
+	public static final String FILE_NAME = "sudo.sav";
+	public static final String HISTORY_NAME = "sudo.his";
+	
+	public static final String HISTORY_NAME_TEST = "test.his";
+	
+	private static final String MESSAGE_HISTORY_LOAD_ERROR = "Loading history file error: file can not be found.";
+	private static final String MESSAGE_LAST_HISTORY = "No more histories. Cannot undo.";
+	
+	public static final int KEYBOARD_SPACE = 32;
+	public static final int KEYBOARD_TAB = 9;
+	
+	public static final HashMap<String, COMMAND_TYPE > aliases = new HashMap<String, COMMAND_TYPE>(){{
+        put("ADD",COMMAND_TYPE.ADD);
+        put("DO",COMMAND_TYPE.ADD);
+        put("SUDO",COMMAND_TYPE.ADD);
+        
+        put("DELETE",COMMAND_TYPE.DELETE);
+        put("DEL",COMMAND_TYPE.DELETE);
+        put("REMOVE",COMMAND_TYPE.DELETE);
+        
+        put("EDIT",COMMAND_TYPE.EDIT);
+        put("CHANGE",COMMAND_TYPE.EDIT);
+        put("MODIFY",COMMAND_TYPE.EDIT);
+        
+        put("FINISH",COMMAND_TYPE.FINISH);
+        put("COMPLETE",COMMAND_TYPE.FINISH);
+        
+        put("SEARCH",COMMAND_TYPE.SEARCH);
+        put("FIND",COMMAND_TYPE.SEARCH);
+        
+        put("DISPLAY",COMMAND_TYPE.DISPLAY);
+        put("SHOW",COMMAND_TYPE.DISPLAY);
+        
+        put("UNDO",COMMAND_TYPE.UNDO);
+        put("REDO",COMMAND_TYPE.REDO);        
+        put("FREE",COMMAND_TYPE.FREE);
+        put("SORT",COMMAND_TYPE.SORT);
+        put("HELP",COMMAND_TYPE.HELP);
+        put("ALL",COMMAND_TYPE.ALL);
+        put("UNFINISH",COMMAND_TYPE.UNFINISH);
+        
+        put("EXIT",COMMAND_TYPE.EXIT);
+        put("QUIT",COMMAND_TYPE.EXIT);
+    }};
+	
+	
+}
---
+++ src/sg/edu/nus/cs2103/sudo/logic/DeadlineTask.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package sg.edu.nus.cs2103.sudo.logic;
+
+import java.util.ArrayList;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeComparator;
+
+import sg.edu.nus.cs2103.sudo.Constants;
+
+/**
+ * @author Ipsita Mohapatra A0101286N
+ * 
+ *         This class stores the information about Deadline Tasks. Deadline
+ *         Tasks will have an id, description, isComplete and endTime. None of
+ *         these fields will be null.
+ * 
+ */
+public class DeadlineTask extends Task {
+
+	private static final int INVALID = -1;
+
+	// Deadline Task constructor
+	public DeadlineTask(String taskDescription, ArrayList<DateTime> dateTimes) {
+		assert (taskDescription != null);
+		assert (dateTimes.size() == 1);
+
+		this.description = taskDescription;
+		DateTime endTime = dateTimes.get(0);
+
+		// checkValidityTimes(endTime);
+
+		this.endTime = endTime;
+	}
+	
+	public DeadlineTask(int id, String taskDescription, boolean isComplete, DateTime endTime) {
+		this.id = id;
+		this.description = taskDescription;
+		this.isComplete = isComplete;
+		this.endTime = endTime;
+	}
+	
+	public String toStringForFile() {
+		int bound = endTime.toString().indexOf(":");
+		String returnedEndTime = this.endTime.toString()
+				.substring(0, bound + 3);
+
+		return "DEADLINE" + "#" + description + "#" + returnedEndTime + "#"
+				+ isComplete;
+	}
+}

9017f288c3490eb5cd165133881d239d7846fa50
Yos Riady <yosriady@gmail.com>
2013-11-01 20:15:26 +0800

    ParserUtils follows java code convention

--- src/sg/edu/nus/cs2103/sudo/logic/ParserUtils.java
+++ src/sg/edu/nus/cs2103/sudo/logic/ParserUtils.java
@@ -11,15 +11,24 @@ import sg.edu.nus.cs2103.sudo.Constants;
 
 import com.joestelmach.natty.DateGroup;
 
+/**
+ * This class contains some static methods to 
+ * parse task information such as ID, description,
+ * and dates.
+ * 
+ * @author Yos Riady
+ * 
+ */
 public class ParserUtils {
 	/**
-	 * Retrieves date List objects from joda-datetime DateGroup objects
-	 * @param ArrayList of DateGroup objects
+	 * Retrieves date List objects from joda-datetime DateGroup objects.
+	 * @param dateGroups	A list of joda-datetime DateGroup objects
 	 * @return ArrayList of Date Lists
 	 */
-	public static ArrayList<List<Date>> getDateLists(List<DateGroup> dateGroups) {
+	public static ArrayList<List<Date>> getDateLists(
+			List<DateGroup> dateGroups) {
 		ArrayList<List<Date>> dateLists = new ArrayList<List<Date>>();
-		for(DateGroup dateGroup:dateGroups) {
+		for (DateGroup dateGroup:dateGroups) {
 			  List<Date> dateList = dateGroup.getDates();
 			  dateLists.add(dateList);
 		}
@@ -28,18 +37,19 @@ public class ParserUtils {
 	}
 	
 	/**
-	 * Convert DateLists to joda-DateTime objects
-	 * @param ArrayList of Date Lists
+	 * Convert DateLists to joda-DateTime objects.
+	 * @param dateLists		ArrayList of Date Lists
 	 * @return ArrayList of DateTime objects
 	 */
-	public static ArrayList<DateTime> convertToDateTimes(ArrayList<List<Date>> dateLists){
+	public static ArrayList<DateTime> convertToDateTimes(
+			ArrayList<List<Date>> dateLists){
 		ArrayList<DateTime> dateTimes = new ArrayList<DateTime>();
-		if(dateLists.isEmpty()){
+		if (dateLists.isEmpty()) {
 			return dateTimes;
 		}
 		
 		List<Date> dates = dateLists.get(0);
-		for(Date date:dates){ //cast Date to joda-DateTime
+		for (Date date:dates) {
 			DateTime dt = new DateTime(date);
 			dateTimes.add(dt);
 		}
@@ -48,8 +58,8 @@ public class ParserUtils {
 	}	
 	
 	/**
-	 * Get the first word (the command word) from the user input
-	 * @param String userInput
+	 * Get the first word (the command word) from the user input.
+	 * @param userInput		the user's input
 	 * @return String first word
 	 */
 	public static String getCommandWord(String userInput) {
@@ -58,20 +68,23 @@ public class ParserUtils {
 	}		
 	
 	/**
-	 * Validates that the first word (command word) is a valid COMMAND_TYPE
-	 * In other words, this method checks if the command is in COMMAND_TYPE
+	 * Validates that the first word (command word) is a valid COMMAND_TYPE.
+	 * In other words, this method checks if the command is in COMMAND_TYPE.
+	 * @param userCommand	The user's command word
+	 * @return COMMAND_TYPE
 	 */		
-	public static COMMAND_TYPE getCommandType(String userCommand){
-		COMMAND_TYPE commandType = Constants.aliases.get(userCommand.toUpperCase());
-		if(commandType == null){
+	public static COMMAND_TYPE getCommandType(String userCommand) {
+		COMMAND_TYPE commandType = Constants.aliases.get(
+				userCommand.toUpperCase());
+		if (commandType == null) {
 			return COMMAND_TYPE.INVALID;
 		}
 		return commandType;	
 	}
 
 	/**
-	 * Count the number of words in a string
-	 * @param String inputString
+	 * Count the number of words in a string.
+	 * @param inputString	A string input
 	 * @return number of words
 	 */
 	public static int countWords(String inputString) {
@@ -84,8 +97,8 @@ public class ParserUtils {
 	
 	/**
 	 * Returns the minimum number of words required
-	 * for a valid command
-	 * @param COMMAND_TYPE commandType
+	 * for a valid command.
+	 * @param commandType
 	 * @return number of words
 	 */
 	public static int getNumOfWordsNeeded(COMMAND_TYPE commandType) {

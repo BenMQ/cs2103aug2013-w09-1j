be757e685a131acb9e5806342a06a75d71da87c0
Ipsita Mohapatra <ipsitamohapatra93@gmail.com>
2013-11-04 23:36:03 +0800

    Some more changes to exceptions.

--- src/sg/edu/nus/cs2103/sudo/logic/LogicHandler.java
+++ src/sg/edu/nus/cs2103/sudo/logic/LogicHandler.java
@@ -11,19 +11,19 @@ import sg.edu.nus.cs2103.sudo.Constants;
 import sg.edu.nus.cs2103.ui.UI;
 
 /**
- * LogicHandler is a facade class responsible for providing
- * a simple API to handle most of the Task operations of sudo. 
+ * LogicHandler is a facade class responsible for providing a simple API to
+ * handle most of the Task operations of sudo.
  * 
- * @author Yos Riady 
+ * @author Yos Riady
  */
 public final class LogicHandler {
 
 	private static LogicHandler logicHandler;
-	
+
 	private Scanner scanner;
 	private TaskManager manager;
 
-	/** 
+	/**
 	 * Constructor to initialize the logicHandler Object.
 	 * 
 	 * @param manager
@@ -33,47 +33,43 @@ public final class LogicHandler {
 	 */
 	private LogicHandler(TaskManager manager, Scanner scanner) {
 		if (manager == null) {
-			throw new NullPointerException(
-					"TaskManager cannot be null!");
+			throw new NullPointerException("TaskManager cannot be null!");
 		}
 		this.manager = manager;
 		this.scanner = scanner;
 	}
-	
+
 	/**
-	 * Returns a singleton instance of logicHandler.
-	 * Uses dependency injection to take in a TaskManager 
-	 * and Scanner object.
+	 * Returns a singleton instance of logicHandler. Uses dependency injection
+	 * to take in a TaskManager and Scanner object.
 	 * 
 	 * @param manager
 	 *            : a TaskManager object to delegate operations to
 	 * @param scanner
 	 *            : a Scanner object that listens for user input
-	 * @return logicHandler
-	 * 			  : a logicHandler object
+	 * @return logicHandler : a logicHandler object
 	 */
-	public static LogicHandler getLogicHandler(TaskManager manager, 
+	public static LogicHandler getLogicHandler(TaskManager manager,
 			Scanner scanner) {
 		if (logicHandler == null) {
 			logicHandler = new LogicHandler(manager, scanner);
 		}
 		return logicHandler;
 	}
-	
-	public TaskManager getManager(){
+
+	public TaskManager getManager() {
 		return this.manager;
 	}
-	
-	public Scanner getScanner(){
+
+	public Scanner getScanner() {
 		return this.scanner;
-	}	
+	}
 
 	/**
-	 * Executes a TaskManager method based 
-	 * on the user's input. 
+	 * Executes a TaskManager method based on the user's input.
 	 * 
 	 * @param userInput
-	 *           : string of the user's input
+	 *            : string of the user's input
 	 */
 	public void executeCommand(final String userInput) {
 		COMMAND_TYPE userCommand = InputParser.
@@ -165,21 +161,23 @@ public final class LogicHandler {
 			System.out.printf(e.getMessage());
 		} catch (UnsupportedOperationException e) {
 			System.out.printf(e.getMessage());
+		} catch (NullPointerException e) {
+			System.out.printf(e.getMessage());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
 	/**
-	 * Delegates delete commands to the
-	 * TaskManager delete API based on the number of
-	 * search results.
+	 * Delegates delete commands to the TaskManager delete API based on the
+	 * number of search results.
 	 * 
 	 * @param taskDescription
 	 * @throws IOException
 	 */
-	public void delegateDelete(String taskDescription, int targetId) throws IOException {
-		if(taskDescription == null){
+	public void delegateDelete(String taskDescription, int targetId)
+			throws IOException {
+		if (taskDescription == null) {
 			this.manager.delete(targetId);
 		} else {
 			int numResults = this.manager.delete(taskDescription);
@@ -192,42 +190,41 @@ public final class LogicHandler {
 	}
 
 	/**
-	 * Delegates to the TaskManager add API 
-	 * based on the number of date arguments.
+	 * Delegates to the TaskManager add API based on the number of date
+	 * arguments.
 	 * 
-	 * @param taskDescription	The task description
-	 * @param dateTimes			A list of DateTimes
+	 * @param taskDescription
+	 *            The task description
+	 * @param dateTimes
+	 *            A list of DateTimes
 	 * @throws Exception
 	 */
 	public void delegateAddTasks(String taskDescription,
-		ArrayList<DateTime> dateTimes) throws Exception {
-		
+			ArrayList<DateTime> dateTimes) throws Exception {
+
 		if (taskDescription == null) {
 			System.out.print(Constants.MESSAGE_MISSING_DESCRIPTION);
 			return;
-		}		
-		
+		}
+
 		assert taskDescription != null;
 		int numOfDates = dateTimes.size();
 		if (numOfDates == 0) {
-				this.manager.addTask(new FloatingTask(taskDescription));
-				System.out.printf(Constants.MESSAGE_ADD_FLOATING, 
-						taskDescription);
+			this.manager.addTask(new FloatingTask(taskDescription));
+			System.out.printf(Constants.MESSAGE_ADD_FLOATING, taskDescription);
 		} else if (numOfDates == 1) {
-				this.manager.addTask(
-						new DeadlineTask(taskDescription, dateTimes));
-				System.out.printf(
-						Constants.MESSAGE_ADD_DEADLINE, taskDescription, 
-								  UI.formatDate(dateTimes.get(0)));
+			this.manager.addTask(new DeadlineTask(taskDescription, dateTimes));
+			System.out.printf(Constants.MESSAGE_ADD_DEADLINE, taskDescription,
+					UI.formatDate(dateTimes.get(0)));
 		} else if (numOfDates == 2) {
-				this.manager.addTask(new TimedTask(taskDescription, dateTimes));
-				System.out.printf(Constants.MESSAGE_ADD_TIMED, taskDescription,
-						UI.formatDate(dateTimes.get(0)), 
-						UI.formatDate(dateTimes.get(1)));
+			this.manager.addTask(new TimedTask(taskDescription, dateTimes));
+			System.out.printf(Constants.MESSAGE_ADD_TIMED, taskDescription,
+					UI.formatDate(dateTimes.get(0)),
+					UI.formatDate(dateTimes.get(1)));
 		} else {
 			System.out.print(Constants.MESSAGE_INVALID_NUMBER_OF_DATES);
 		}
 		this.manager.displayAllTasks();
 	}
-	
+
 }

e113ad648355eb7cf9ea6126165f7c0c2c18dbd8
Yos Riady <yosriady@gmail.com>
2013-11-04 12:56:58 +0800

    Add unit tests, refactor aliases

--- src/sg/edu/nus/cs2103/sudo/Constants.java
+++ src/sg/edu/nus/cs2103/sudo/Constants.java
@@ -1,8 +1,5 @@
 package sg.edu.nus.cs2103.sudo;
 
-import java.util.HashMap;
-
-
 public class Constants {
 
 	public static final String MESSAGE_INVALID_COMMAND = "Invalid command\n";
@@ -56,49 +53,5 @@ public class Constants {
 
 	public static final String FINISHED_TASK_SEPARATOR = "\n[Finished tasks]=======================";
 	public static final String FLOATING_TASK_SEPARATOR = "\n[Floating tasks]=======================";	
-	
-	
-	public static final HashMap<String, COMMAND_TYPE> aliases = new HashMap<String, COMMAND_TYPE>() {
-		/**
-		 *  This key-value table maps aliases to their corresponding COMMAND_TYPE.
-		 */
-		private static final long serialVersionUID = 1L;
-		{
-			put("ADD", COMMAND_TYPE.ADD);
-			put("DO", COMMAND_TYPE.ADD);
-			put("SUDO", COMMAND_TYPE.ADD);
-
-			put("DELETE", COMMAND_TYPE.DELETE);
-			put("DEL", COMMAND_TYPE.DELETE);
-			put("REMOVE", COMMAND_TYPE.DELETE);
-
-			put("EDIT", COMMAND_TYPE.EDIT);
-			put("CHANGE", COMMAND_TYPE.EDIT);
-			put("MODIFY", COMMAND_TYPE.EDIT);
-
-			put("FINISH", COMMAND_TYPE.FINISH);
-			put("COMPLETE", COMMAND_TYPE.FINISH);
-			put("DONE", COMMAND_TYPE.FINISH);
-
-			put("SEARCH", COMMAND_TYPE.SEARCH);
-			put("FIND", COMMAND_TYPE.SEARCH);
-
-			put("DISPLAY", COMMAND_TYPE.DISPLAY);
-			put("SHOW", COMMAND_TYPE.DISPLAY);
-
-			put("UNDO", COMMAND_TYPE.UNDO);
-			put("REDO", COMMAND_TYPE.REDO);
-			put("FREE", COMMAND_TYPE.FREE);
-			put("SCHEDULE", COMMAND_TYPE.SCHEDULE);
-			put("SORT", COMMAND_TYPE.SORT);
-			put("HELP", COMMAND_TYPE.HELP);
-			put("ALL", COMMAND_TYPE.ALL);
-			put("UNFINISH", COMMAND_TYPE.UNFINISH);
-
-			put("EXIT", COMMAND_TYPE.EXIT);
-			put("QUIT", COMMAND_TYPE.EXIT);
-			
-			put("DESTROY", COMMAND_TYPE.DESTROY);
-		}
-	};	
+		
 }

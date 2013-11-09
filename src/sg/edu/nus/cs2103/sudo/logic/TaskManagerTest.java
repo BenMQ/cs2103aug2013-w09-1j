d8d3fefa432a361421bdd7e4b25b11b0eb0c1cbc
Minqi Chen <cnchenminqi@gmail.com>
2013-11-09 11:22:24 +0800

    Rewrote free interval tests

--- src/sg/edu/nus/cs2103/sudo/logic/TaskManagerTest.java
+++ src/sg/edu/nus/cs2103/sudo/logic/TaskManagerTest.java
@@ -340,7 +340,7 @@ public class TaskManagerTest {
     }
     
 	@Test
-	public void testSearchForFreeIntervalsNoTask() throws Exception {
+	public void testGetFreeIntervalsNoTask() throws Exception {
 		DateTime now = new DateTime();
 		DateTime dt0000 = new DateTime(now.getYear(), now.getMonthOfYear(),
 				now.getDayOfMonth(), 0, 0, 0);
@@ -350,19 +350,15 @@ public class TaskManagerTest {
 		range.add(dt0000);
 		range.add(dt2359);
 		// No task
-		manager.searchForFreeIntervals(range);
-		assertEquals(
-				Constants.MESSAGE_FREE_SLOTS_PREFIX
-						+ range.get(0).toString("dd MMMM hh:mm a") + " to "
-						+ range.get(1).toString("dd MMMM hh:mm a") + "\n"
-						+ dt0000.toString("dd MMMM hh:mm a") + " to "
-						+ dt2359.toString("dd MMMM hh:mm a") + "\n",
-				outContent.toString());
-		outContent.reset();
+		ArrayList<MutableInterval> actual = manager.getFreeIntervals(range);
+		ArrayList<MutableInterval> expected = new ArrayList<MutableInterval>();
+		expected.add(new MutableInterval(dt0000, dt2359));
+		
+		assertEquals(expected, actual);
 	}
 
 	@Test
-	public void testSearchForFreeIntervalsSomeTasks() throws Exception {
+	public void testGetFreeIntervalsSomeTasks() throws Exception {
 		DateTime now = new DateTime();
 		DateTime dt0000 = new DateTime(now.getYear(), now.getMonthOfYear(),
 				now.getDayOfMonth(), 0, 0, 0);
@@ -376,44 +372,16 @@ public class TaskManagerTest {
 				12, 0)));
 		manager.addTask(new TimedTask(0, "timed", false, today(17, 0), today(
 				18, 0)));
-		manager.searchForFreeIntervals(range);
-		assertEquals(
-				Constants.MESSAGE_FREE_SLOTS_PREFIX
-						+ range.get(0).toString("dd MMMM hh:mm a") + " to "
-						+ range.get(1).toString("dd MMMM hh:mm a") + "\n"
-						+ today(0, 0).toString("dd MMMM hh:mm a") + " to "
-						+ today(11, 0).toString("dd MMMM hh:mm a") + "\n"
-						+ today(12, 0).toString("dd MMMM hh:mm a") + " to "
-						+ today(17, 0).toString("dd MMMM hh:mm a") + "\n"
-						+ today(18, 0).toString("dd MMMM hh:mm a") + " to "
-						+ today(23, 59).toString("dd MMMM hh:mm a") + "\n",
-				outContent.toString());
-		outContent.reset();
+		ArrayList<MutableInterval> actual = manager.getFreeIntervals(range);
+		ArrayList<MutableInterval> expected = new ArrayList<MutableInterval>();
+        expected.add(new MutableInterval(dt0000, today(11, 0)));
+        expected.add(new MutableInterval(today(12, 0), today(17, 0)));
+        expected.add(new MutableInterval(today(18, 0), dt2359));
+		assertEquals(expected, actual);
 	}
 
 	@Test
-	public void testSearchForFreeIntervalsSmallIntervalOnly() throws Exception {
-		DateTime now = new DateTime();
-		DateTime dt0000 = new DateTime(now.getYear(), now.getMonthOfYear(),
-				now.getDayOfMonth(), 0, 0, 0);
-		DateTime dt2359 = new DateTime(now.getYear(), now.getMonthOfYear(),
-				now.getDayOfMonth(), 23, 59, 59);
-		ArrayList<DateTime> range = new ArrayList<DateTime>();
-		range.add(dt0000);
-		range.add(dt2359);
-		// almost whole day
-		manager.addTask(new TimedTask(0, "timed", false, today(0, 0), today(12,
-				0)));
-		manager.addTask(new TimedTask(0, "timed", false, today(12, 1), dt2359));
-		manager.searchForFreeIntervals(range);
-		assertEquals(Constants.MESSAGE_NO_FREE_SLOTS + "\n",
-				outContent.toString());
-		outContent.reset();
-
-	}
-
-	@Test
-	public void testSearchForFreeIntervalsWholeDay() throws Exception {
+	public void testGetFreeIntervalsWholeDay() throws Exception {
 		DateTime now = new DateTime();
 		DateTime dt0000 = new DateTime(now.getYear(), now.getMonthOfYear(),
 				now.getDayOfMonth(), 0, 0, 0);
@@ -424,12 +392,10 @@ public class TaskManagerTest {
 		range.add(dt2359);
 		// whole day
 		manager.addTask(new TimedTask(0, "timed", false, dt0000, dt2359));
-		manager.searchForFreeIntervals(range);
-		assertEquals(Constants.MESSAGE_NO_FREE_SLOTS + "\n",
-				outContent.toString());
-		outContent.reset();
-
-	}
+		ArrayList<MutableInterval> actual = manager.getFreeIntervals(range);
+		ArrayList<MutableInterval> expected = new ArrayList<MutableInterval>();
+        assertEquals(expected, actual);
+    }
 
 	private String displayTasks(ArrayList<Task> tasks) {
 

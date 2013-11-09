e4a8354bdacd9249ad0a7a4a90e04222af3fa2b6
lolitacon2012 <lolitacon2012@localhost>
2013-11-09 14:00:30 +0800

    minor fixes

---
+++ src/sg/edu/nus/cs2103/sudo/logic/Task.java
@@ -0,0 +1,112 @@
+package sg.edu.nus.cs2103.sudo.logic;
+
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeComparator;
+
+import sg.edu.nus.cs2103.sudo.Constants;
+
+/**
+ * @author Ipsita Mohapatra A0101286N
+ *
+ * This class stores the information about all types of Tasks.
+ * This abstract class is the parent class of TimedTask, DeadlineTask and FloatingTask
+ * Some of the fields like endTime and/or startTime may be empty.
+ * 
+ */
+
+public abstract class Task {
+	protected int id;
+	protected String description = "";
+	protected Boolean isComplete = false;
+	protected DateTime startTime;
+	protected DateTime endTime;
+	
+	public Task() {}
+	
+	public Task(int id, String description, DateTime startTime, DateTime endTime) {
+	    this.id = id;
+	    this.description = description;
+	    this.startTime = startTime;
+	    this.endTime = endTime;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+
+	public DateTime getStartTime() {
+		return startTime;
+	}
+	
+	public DateTime getEndTime() {
+		return endTime;
+	}	
+	
+	public boolean isComplete() {
+		return isComplete;
+	}
+	
+	public boolean isFloatingTask() {
+		return this.startTime == null && this.endTime == null;
+	}
+
+	public boolean isDeadlineTask() {
+		return this.getEndTime() != null;
+	}
+
+	public boolean isTimedTask() {
+		return this.getStartTime() != null && this.isDeadlineTask();
+	}
+	
+	public boolean isOnSameDay(){
+		return this.getStartTime().getDayOfYear() == this.getEndTime().getDayOfYear();
+	}
+	
+	/**
+	 * Converts the task object into a string with the following format:
+	 * 1) Timed task: desc from x to y
+	 * 2) Deadline task: desc by x
+	 * 3) Floating task: desc
+	 */
+	public String toString() {
+		String output = id + ". " + description;
+	    if (startTime != null) {
+	        output += " from " + startTime.toString("EEE dd MMMM hh:mm a") + " to " + endTime.toString("EEE dd MMMM hh:mm a");
+	    } else if (endTime != null) {
+	        output += " by " + endTime.toString("EEE dd MMMM hh:mm a");
+	    }
+	    
+	    return output;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public void setDescription(String newDescription) {
+		description = newDescription;
+	}
+	
+	public void setStartTime(DateTime startTime) {
+		this.startTime = startTime;
+	}
+	
+	public void setEndTime(DateTime endTime) {
+		this.endTime = endTime;
+	}
+	
+	public void setComplete(Boolean isComplete) {
+		this.isComplete = isComplete;
+	}
+	
+	public abstract String toStringForFile();
+	
+	public abstract String getAddMessage();
+	
+	public abstract String getDisplayString();
+	
+}

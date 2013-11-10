99d10bc0a50b580e270d40ee8998ff2f64cab846
Ipsita Mohapatra <ipsitamohapatra93@gmail.com>
2013-11-10 23:38:38 +0800

    Refactor TaskManagerTest

--- src/sg/edu/nus/cs2103/sudo/logic/TaskManagerTest.java
+++ src/sg/edu/nus/cs2103/sudo/logic/TaskManagerTest.java
@@ -32,7 +32,8 @@ public class TaskManagerTest {
 	private File savefile;
 
 	private final static String floatingTaskDescription = "learn how to fish";
-	private final static String timedTaskDescription = "Trying an invalid time interval";
+	private final static String timedTaskDescription = "Trying an invalid"
+			+ " time interval";
 
 	@Before
 	public void setUp() throws FileNotFoundException {
@@ -49,12 +50,6 @@ public class TaskManagerTest {
 		savefile.delete();
 	}
 
-	/**
-	 * Tests adding floating tasks, timed tasks and deadline tasks and
-	 * displaying of all uncompleted floating tasks.
-	 * 
-	 * @throws Exception
-	 */
 	@Test
 	public void testAddTasks() throws Exception {
 		// adding valid floating task
@@ -68,7 +63,8 @@ public class TaskManagerTest {
 
 		// boundary case: add task with invalid time interval
 		try {
-			manager.addTaskAndSort(new TimedTask(timedTaskDescription, dateTimes));
+			manager.addTaskAndSort(new TimedTask(timedTaskDescription,
+					dateTimes));
 		} catch (Exception e) {
 			assertEquals(Constants.MESSAGE_END_BEFORE_START_TIME,
 					e.getMessage());
@@ -78,40 +74,39 @@ public class TaskManagerTest {
 		dateTimes.clear();
 		dateTimes.add(new DateTime());
 		dateTimes.add(new DateTime());
-		
+
 		// boundary case: adding the same start and end time
 		/*
-		try {
-			manager.addTask(new TimedTask("Trying same start and end time",
-					dateTimes));
-		} catch (Exception e) {
-			assertEquals(Constants.MESSAGE_SAME_START_END_TIME, e.getMessage());
-		}
-		*/
-		
-		manager.addTaskAndSort(new TimedTask(0, "Have dinner with family in NUS",
-				false, new DateTime(2013, 10, 23, 19, 0, 0, 0), new DateTime(
-						2013, 10, 23, 21, 0, 0, 0)));
+		 * try { manager.addTask(new TimedTask("Trying same start and end time",
+		 * dateTimes)); } catch (Exception e) {
+		 * assertEquals(Constants.MESSAGE_SAME_START_END_TIME, e.getMessage());
+		 * }
+		 */
+
+		manager.addTaskAndSort(new TimedTask(0,
+				"Have dinner with family in NUS", false, new DateTime(2013, 10,
+						23, 19, 0, 0, 0), new DateTime(2013, 10, 23, 21, 0, 0,
+						0)));
 		assertEquals(
-				"1. Have dinner with family in NUS from Wed 23 October 07:00 PM" +
-				" to Wed 23 October 09:00 PM false\n2. learn how to fish false\n",
+				"1. Have dinner with family in NUS from Wed 23 October 07:00 PM"
+						+ " to Wed 23 October 09:00 PM false\n"
+						+ "2. learn how to fish false\n",
 				displayTasks(manager.getTasks()));
 
 		// adding valid deadline task
-		manager.addTaskAndSort(new DeadlineTask(0, "Submit proposal at Nus", false,
-				new DateTime(2013, 10, 21, 0, 0, 0, 0)));
+		manager.addTaskAndSort(new DeadlineTask(0, "Submit proposal at Nus",
+				false, new DateTime(2013, 10, 21, 0, 0, 0, 0)));
 		assertEquals(
-				"1. Submit proposal at Nus by Mon 21 October 12:00 AM false\n" +
-				"2. Have dinner with family in NUS from Wed 23 October 07:00 PM" +
-				" to Wed 23 October 09:00 PM false\n3. learn how to fish false\n",
+				"1. Submit proposal at Nus by Mon 21 October 12:00 AM false\n"
+						+ "2. Have dinner with family in NUS from "
+						+ "Wed 23 October 07:00 PM"
+						+ " to Wed 23 October 09:00 PM false\n"
+						+ "3. learn how to fish false\n",
 				displayTasks(manager.getTasks()));
 	}
 
-	/**
-	 * @throws Exception
-	 */
 	@Test
-	public void testFinishTasks() throws Exception {
+	public void testFinishTasksInvalid() throws Exception {
 		testAddTasks();
 
 		// Equivalence partitioning
@@ -138,12 +133,19 @@ public class TaskManagerTest {
 			assertEquals(Constants.MESSAGE_INVALID_TASK_INDEX, e.getMessage());
 		}
 
+	}
+
+	public void testFinishTasksValid() throws Exception {
+		testAddTasks();
+
 		// valid task id
 		manager.markAsComplete(2);
 		assertEquals(
-				"1. Submit proposal at Nus by Mon 21 October 12:00 AM false\n" +
-				"2. learn how to fish false\n3. Have dinner with family in NUS " +
-				"from Wed 23 October 07:00 PM to Wed 23 October 09:00 PM true\n",
+				"1. Submit proposal at Nus by Mon 21 October 12:00 AM false\n"
+						+ "2. learn how to fish false\n"
+						+ "3. Have dinner with family in NUS "
+						+ "from Wed 23 October 07:00 PM to Wed "
+						+ "23 October 09:00 PM true\n",
 				displayTasks(manager.getTasks()));
 	}
 
@@ -151,9 +153,11 @@ public class TaskManagerTest {
 	public void testEditTasks() throws Exception {
 		testAddTasks();
 		assertEquals(
-				"1. Submit proposal at Nus by Mon 21 October 12:00 AM false\n" +
-				"2. Have dinner with family in NUS from Wed 23 October 07:00 PM" +
-				" to Wed 23 October 09:00 PM false\n3. learn how to fish false\n",
+				"1. Submit proposal at Nus by Mon 21 October 12:00 AM false\n"
+						+ "2. Have dinner with family in NUS from"
+						+ " Wed 23 October 07:00 PM"
+						+ " to Wed 23 October 09:00 PM false\n"
+						+ "3. learn how to fish false\n",
 				displayTasks(manager.getTasks()));
 
 		// boundary case where number of dateTimes taken in by method is 0
@@ -162,10 +166,11 @@ public class TaskManagerTest {
 		manager.editTask(3, "learn how to fish with dad",
 				new ArrayList<DateTime>());
 		assertEquals(
-				"1. Submit proposal at Nus by Mon 21 October 12:00 AM false\n" +
-				"2. Have dinner with family in NUS from Wed 23 October 07:00 PM " +
-				"to Wed 23 October 09:00 PM false\n" +
-				"3. learn how to fish with dad false\n",
+				"1. Submit proposal at Nus by Mon 21 October 12:00 AM false\n"
+						+ "2. Have dinner with family in NUS from "
+						+ "Wed 23 October 07:00 PM "
+						+ "to Wed 23 October 09:00 PM false\n"
+						+ "3. learn how to fish with dad false\n",
 				displayTasks(manager.getTasks()));
 
 		// boundary case for taskId; invalid task id
@@ -181,21 +186,22 @@ public class TaskManagerTest {
 		dateTimes.add(new DateTime(2013, 10, 22, 9, 0, 0, 0));
 		manager.editTask(1, "", dateTimes);
 		assertEquals(
-				"1. Submit proposal at Nus by Tue 22 October 09:00 AM false\n" +
-				"2. Have dinner with family in NUS from Wed 23 October 07:00 PM " +
-				"to Wed 23 October 09:00 PM false\n" +
-				"3. learn how to fish with dad false\n",
+				"1. Submit proposal at Nus by Tue 22 October 09:00 AM false\n"
+						+ "2. Have dinner with family in NUS from Wed 23 October 07:00 PM "
+						+ "to Wed 23 October 09:00 PM false\n"
+						+ "3. learn how to fish with dad false\n",
 				displayTasks(manager.getTasks()));
 
 		// boundary case: number of dateTimes is 2 (timed task)
 		dateTimes.add(new DateTime(2013, 10, 22, 17, 0, 0, 0));
 		manager.editTask(1, "", dateTimes);
 		assertEquals(
-				"1. Submit proposal at Nus from Tue 22 October 09:00 AM to " +
-				"Tue 22 October 05:00 PM false\n" +
-				"2. Have dinner with family in NUS from Wed 23 October 07:00 PM " +
-				"to Wed 23 October 09:00 PM false\n" +
-				"3. learn how to fish with dad false\n",
+				"1. Submit proposal at Nus from Tue 22 October 09:00 AM to "
+						+ "Tue 22 October 05:00 PM false\n"
+						+ "2. Have dinner with family in NUS from "
+						+ "Wed 23 October 07:00 PM "
+						+ "to Wed 23 October 09:00 PM false\n"
+						+ "3. learn how to fish with dad false\n",
 				displayTasks(manager.getTasks()));
 
 	}
@@ -207,22 +213,28 @@ public class TaskManagerTest {
 	 * @throws Exception
 	 */
 	@Test
-	public void testSearchTasks() throws Exception {
+	public void testSearchTasksValid() throws Exception {
 		testAddTasks();
 
 		assertEquals(
-				"2. Have dinner with family in NUS from Wed 23 October 07:00 PM " +
-				"to Wed 23 October 09:00 PM false\n",
+				"2. Have dinner with family in NUS from Wed 23 October 07:00 PM "
+						+ "to Wed 23 October 09:00 PM false\n",
 				displayTasks(manager.search("family", true)));
-		
+
 		assertEquals(
-				"1. Submit proposal at Nus by Mon 21 October 12:00 AM false\n" +
-				"2. Have dinner with family in NUS from Wed 23 October 07:00 PM" +
-				" to Wed 23 October 09:00 PM false\n",
+				"1. Submit proposal at Nus by Mon 21 October 12:00 AM false\n"
+						+ "2. Have dinner with family in NUS from "
+						+ "Wed 23 October 07:00 PM"
+						+ " to Wed 23 October 09:00 PM false\n",
 				displayTasks(manager.search("nus", false)));
-		
+
 		assertEquals("Nothing to display.\n",
 				displayTasks(manager.search("fishes", true)));
+
+	}
+
+	public void testSearchEmptyString() throws Exception {
+		testAddTasks();
 
 		// boundary case: search string is empty
 		try {
@@ -230,22 +242,18 @@ public class TaskManagerTest {
 					displayTasks(manager.search("", true)));
 		} catch (Exception e) {
 			assertEquals(Constants.MESSAGE_INVALID_SEARCH, e.getMessage());
-		}	
-
+		}
 	}
 
-	/**
-	 * Tests delete with task id as well as delete with search string
-	 * 
-	 * @throws Exception
-	 */
 	@Test
-	public void testDelete() throws Exception {
+	public void testDeleteInvalidId() throws Exception {
 		testAddTasks();
 		assertEquals(
-				"1. Submit proposal at Nus by Mon 21 October 12:00 AM false\n" +
-				"2. Have dinner with family in NUS from Wed 23 October 07:00 PM " +
-				"to Wed 23 October 09:00 PM false\n3. learn how to fish false\n",
+				"1. Submit proposal at Nus by Mon 21 October 12:00 AM false\n"
+						+ "2. Have dinner with family in NUS from "
+						+ "Wed 23 October 07:00 PM "
+						+ "to Wed 23 October 09:00 PM false\n"
+						+ "3. learn how to fish false\n",
 				displayTasks(manager.getTasks()));
 
 		// boundary case: task id is 0
@@ -255,109 +263,121 @@ public class TaskManagerTest {
 			assertEquals(Constants.MESSAGE_INVALID_TASK_INDEX, e.getMessage());
 		}
 
+	}
+
+	@Test
+	public void testDeleteOneSearchResult() throws Exception {
+		testAddTasks();
+		assertEquals(
+				"1. Submit proposal at Nus by Mon 21 October 12:00 AM false\n"
+						+ "2. Have dinner with family in NUS from Wed 23 October 07:00 PM "
+						+ "to Wed 23 October 09:00 PM false\n"
+						+ "3. learn how to fish false\n",
+				displayTasks(manager.getTasks()));
+
 		// equivalence partitioning: when search results of delete is 1
 		manager.delete("fish");
 		assertEquals("Deleted: learn how to fish\n", outContent.toString());
 		outContent.reset();
-		
+
 		assertEquals(
-				"1. Submit proposal at Nus by Mon 21 October 12:00 AM false\n" +
-				"2. Have dinner with family in NUS from Wed 23 October 07:00 PM" +
-				" to Wed 23 October 09:00 PM false\n",
+				"1. Submit proposal at Nus by Mon 21 October 12:00 AM false\n"
+						+ "2. Have dinner with family in NUS from Wed 23 October 07:00 PM"
+						+ " to Wed 23 October 09:00 PM false\n",
 				displayTasks(manager.getTasks()));
 
 	}
-	
-	//@author A0099314Y 
+
+	// @author A0099314Y
 	@Test
 	public void testGetFlexibleTimeRangeEmpty() {
-	    ArrayList<DateTime> input = new ArrayList<DateTime>();
-	    ArrayList<DateTime> actual = 
-	            TaskManagerUtils.getFlexibleTimeRange(input); 
-	    DateTime dt0000 = today(0, 0);
-        DateTime dt2359 = today(23, 59, 59);
-        ArrayList<DateTime> expected = new ArrayList<DateTime>();
-        expected.add(dt0000);
-        expected.add(dt2359);
-	    assertEquals(expected, actual);
+		ArrayList<DateTime> input = new ArrayList<DateTime>();
+		ArrayList<DateTime> actual = TaskManagerUtils
+				.getFlexibleTimeRange(input);
+		DateTime dt0000 = today(0, 0);
+		DateTime dt2359 = today(23, 59, 59);
+		ArrayList<DateTime> expected = new ArrayList<DateTime>();
+		expected.add(dt0000);
+		expected.add(dt2359);
+		assertEquals(expected, actual);
 	}
-	
-	@Test
-    public void testGetFlexibleTimeRangeOneInput() {
-        DateTime dt0000 = today(0, 0);
-        DateTime dt2359 = today(23, 59, 59);
-        
-        ArrayList<DateTime> input = new ArrayList<DateTime>();
-        input.add(dt2359);
-        
-        ArrayList<DateTime> actual = 
-                TaskManagerUtils.getFlexibleTimeRange(input); 
-        
-        ArrayList<DateTime> expected = new ArrayList<DateTime>();
-        expected.add(dt0000);
-        expected.add(dt2359);
-        assertEquals(expected, actual);
-    }
-    
-	@Test
-    public void testGetFlexibleTimeRangeTwoInputs() {
-        DateTime tmr1300 = today(13, 0).plusDays(1);
-        DateTime dt2359 = today(23, 59, 59);
-        
-        ArrayList<DateTime> input = new ArrayList<DateTime>();
-        input.add(dt2359);
-        input.add(tmr1300);
-        
-        ArrayList<DateTime> actual = 
-                TaskManagerUtils.getFlexibleTimeRange(input); 
-        
-        ArrayList<DateTime> expected = new ArrayList<DateTime>();
-        expected.add(dt2359);
-        expected.add(tmr1300);
-        assertEquals(expected, actual);
-    }
-	
-	@Test
-    public void testGetFlexibleTimeRangeTwoInputsReversed() {
-        DateTime tmr1300 = today(13, 0).plusDays(1);
-        DateTime dt2359 = today(23, 59, 59);
-        
-        ArrayList<DateTime> input = new ArrayList<DateTime>();
 
-        input.add(tmr1300);
-        input.add(dt2359);
-        
-        ArrayList<DateTime> actual = 
-                TaskManagerUtils.getFlexibleTimeRange(input); 
-        
-        ArrayList<DateTime> expected = new ArrayList<DateTime>();
-        expected.add(dt2359);
-        expected.add(tmr1300);
-        assertEquals(expected, actual);
-    }
-	
 	@Test
-    public void testGetFlexibleTimeRangeThreeInputs() {
+	public void testGetFlexibleTimeRangeOneInput() {
+		DateTime dt0000 = today(0, 0);
+		DateTime dt2359 = today(23, 59, 59);
 
-        DateTime dt0800 = today(8, 0);
-        DateTime dt0900 = today(9, 0);
-        DateTime dt2359 = today(23, 59, 59);
-        ArrayList<DateTime> input = new ArrayList<DateTime>();
+		ArrayList<DateTime> input = new ArrayList<DateTime>();
+		input.add(dt2359);
 
-        input.add(dt0800);
-        input.add(dt0900);
-        input.add(dt2359);
-        
-        try {
-            @SuppressWarnings("unused")
-            ArrayList<DateTime> actual = 
-                    TaskManagerUtils.getFlexibleTimeRange(input); 
-            assertTrue(false); // unreachable, assertion should always fail
-        } catch (AssertionError e) {
-            assertTrue(true);
-        }
-    }
-    
+		ArrayList<DateTime> actual = TaskManagerUtils
+				.getFlexibleTimeRange(input);
+
+		ArrayList<DateTime> expected = new ArrayList<DateTime>();
+		expected.add(dt0000);
+		expected.add(dt2359);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testGetFlexibleTimeRangeTwoInputs() {
+		DateTime tmr1300 = today(13, 0).plusDays(1);
+		DateTime dt2359 = today(23, 59, 59);
+
+		ArrayList<DateTime> input = new ArrayList<DateTime>();
+		input.add(dt2359);
+		input.add(tmr1300);
+
+		ArrayList<DateTime> actual = TaskManagerUtils
+				.getFlexibleTimeRange(input);
+
+		ArrayList<DateTime> expected = new ArrayList<DateTime>();
+		expected.add(dt2359);
+		expected.add(tmr1300);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testGetFlexibleTimeRangeTwoInputsReversed() {
+		DateTime tmr1300 = today(13, 0).plusDays(1);
+		DateTime dt2359 = today(23, 59, 59);
+
+		ArrayList<DateTime> input = new ArrayList<DateTime>();
+
+		input.add(tmr1300);
+		input.add(dt2359);
+
+		ArrayList<DateTime> actual = TaskManagerUtils
+				.getFlexibleTimeRange(input);
+
+		ArrayList<DateTime> expected = new ArrayList<DateTime>();
+		expected.add(dt2359);
+		expected.add(tmr1300);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testGetFlexibleTimeRangeThreeInputs() {
+
+		DateTime dt0800 = today(8, 0);
+		DateTime dt0900 = today(9, 0);
+		DateTime dt2359 = today(23, 59, 59);
+		ArrayList<DateTime> input = new ArrayList<DateTime>();
+
+		input.add(dt0800);
+		input.add(dt0900);
+		input.add(dt2359);
+
+		try {
+			@SuppressWarnings("unused")
+			ArrayList<DateTime> actual = TaskManagerUtils
+					.getFlexibleTimeRange(input);
+			assertTrue(false); // unreachable, assertion should always fail
+		} catch (AssertionError e) {
+			assertTrue(true);
+		}
+	}
+
 	@Test
 	public void testGetFreeIntervalsNoTask() throws Exception {
 		DateTime now = new DateTime();
@@ -372,7 +392,7 @@ public class TaskManagerTest {
 		ArrayList<MutableInterval> actual = manager.getFreeIntervals(range);
 		ArrayList<MutableInterval> expected = new ArrayList<MutableInterval>();
 		expected.add(new MutableInterval(dt0000, dt2359));
-		
+
 		assertEquals(expected, actual);
 	}
 
@@ -387,15 +407,15 @@ public class TaskManagerTest {
 		range.add(dt0000);
 		range.add(dt2359);
 		// some slots
-		manager.addTaskAndSort(new TimedTask(0, "timed", false, today(11, 0), today(
-				12, 0)));
-		manager.addTaskAndSort(new TimedTask(0, "timed", false, today(17, 0), today(
-				18, 0)));
+		manager.addTaskAndSort(new TimedTask(0, "timed", false, today(11, 0),
+				today(12, 0)));
+		manager.addTaskAndSort(new TimedTask(0, "timed", false, today(17, 0),
+				today(18, 0)));
 		ArrayList<MutableInterval> actual = manager.getFreeIntervals(range);
 		ArrayList<MutableInterval> expected = new ArrayList<MutableInterval>();
-        expected.add(new MutableInterval(dt0000, today(11, 0)));
-        expected.add(new MutableInterval(today(12, 0), today(17, 0)));
-        expected.add(new MutableInterval(today(18, 0), dt2359));
+		expected.add(new MutableInterval(dt0000, today(11, 0)));
+		expected.add(new MutableInterval(today(12, 0), today(17, 0)));
+		expected.add(new MutableInterval(today(18, 0), dt2359));
 		assertEquals(expected, actual);
 	}
 
@@ -413,8 +433,8 @@ public class TaskManagerTest {
 		manager.addTaskAndSort(new TimedTask(0, "timed", false, dt0000, dt2359));
 		ArrayList<MutableInterval> actual = manager.getFreeIntervals(range);
 		ArrayList<MutableInterval> expected = new ArrayList<MutableInterval>();
-        assertEquals(expected, actual);
-    }
+		assertEquals(expected, actual);
+	}
 
 	private String displayTasks(ArrayList<Task> tasks) {
 
@@ -439,8 +459,8 @@ public class TaskManagerTest {
 		return new DateTime(now.getYear(), now.getMonthOfYear(),
 				now.getDayOfMonth(), hours, minutes, seconds);
 	}
-	
+
 	private DateTime today(int hours, int minutes) {
-	    return today(hours, minutes, 0);
+		return today(hours, minutes, 0);
 	}
 }

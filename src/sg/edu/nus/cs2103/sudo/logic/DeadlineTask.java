134647f30e71dfa7d0f70f5fd2ceb75300d13a67
Yos Riady <yosriady@gmail.com>
2013-11-10 16:41:17 +0800

    Refactor Constants, fix IntegrationTests

---
+++ src/sg/edu/nus/cs2103/sudo/logic/DeadlineTask.java
@@ -0,0 +1,74 @@
+/**
+ * 
+ */
+package sg.edu.nus.cs2103.sudo.logic;
+
+import java.util.ArrayList;
+
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormatter;
+
+import sg.edu.nus.cs2103.sudo.Constants;
+import sg.edu.nus.cs2103.ui.DisplayUtils;
+
+//@author A0101286N
+/**
+ * @author Ipsita Mohapatra 
+ * 
+ *         This class stores the information about Deadline Tasks. Deadline
+ *         Tasks will have an id, description, isComplete and endTime. None of
+ *         these fields will be null.
+ * 
+ */
+public class DeadlineTask extends Task {
+
+	// Deadline Task constructor
+	public DeadlineTask(String taskDescription, ArrayList<DateTime> dateTimes) {
+		assert (taskDescription != null);
+		assert (dateTimes.size() == 1);
+
+		this.description = taskDescription;
+		DateTime endTime = dateTimes.get(0);
+
+		this.endTime = endTime;
+	}
+	
+	public DeadlineTask(int id, String taskDescription, boolean isComplete, DateTime endTime) {
+		this.id = id;
+		this.description = taskDescription;
+		this.isComplete = isComplete;
+		this.endTime = endTime;
+	}
+	
+	public String toStringForFile() {
+		int bound = endTime.toString().indexOf(":");
+		String returnedEndTime = this.endTime.toString()
+				.substring(0, bound + 3);
+
+		return "DEADLINE" + "#" + description + "#" + returnedEndTime + "#"
+				+ isComplete;
+	}
+	
+	//@author A0099317U
+	public String getAddMessage() {
+		return String.format(Constants.MESSAGE_ADD_DEADLINE, 
+				this.description,
+				DisplayUtils.formatDate(this.endTime));
+	}
+
+	/**
+	 * Helper method to return a formatted string for Deadline tasks.
+	 * @param Task
+	 * @return String
+	 */
+	public String getDateString() {
+		DateTimeFormatter onlytimeformat = Constants.HOUR_FORMAT;
+		if (DateTimeUtils.hasZeroMinutes(this.getEndTime())) {
+			onlytimeformat = Constants.HOUR_MINUTE_FORMAT;
+		}		
+		
+		return new StringBuilder(this.getId() + ". [by " + this.getEndTime().
+				toString(onlytimeformat) + "] ").toString();
+	}
+	
+}
---
+++ src/sg/edu/nus/cs2103/sudo/logic/InputParser.java
@@ -0,0 +1,176 @@
+package sg.edu.nus.cs2103.sudo.logic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import sg.edu.nus.cs2103.sudo.COMMAND_TYPE;
+
+import org.joda.time.DateTime;
+
+import com.joestelmach.natty.DateGroup;
+import com.joestelmach.natty.Parser;
+
+//@author A0099317U
+/**
+ * This InputParser class is responsible for extracting commands
+ * and description/time parameters from user string inputs.
+ * 
+ * @author Yos Riady 
+ */
+public class InputParser {
+	
+	/** Flag used when a Task of specified is not found. */
+    public static final int NOT_FOUND = Integer.MIN_VALUE;
+
+	/**
+	 * Parses dates from the user's input string.
+	 * 
+	 * @param userInput 			string of the user's input
+	 * @return ArrayList<DateTime>	A list of DateTime objects
+	 */	
+	public static ArrayList<DateTime> parseDateTime(String userInput,
+	        COMMAND_TYPE command) {
+		Parser dtparser = new Parser();
+		String dateTimeString = getDateTimeString(userInput, command);
+		List<DateGroup> dateGroups = dtparser.parse(dateTimeString);
+		ArrayList<List<Date>> dateLists = InputParserUtils.getDateLists(dateGroups);	
+		ArrayList<DateTime> dateTimes = InputParserUtils.
+				convertToDateTimes(dateLists);
+		
+		return dateTimes;
+	}
+
+	/**
+	 * Reads the user input for command.
+	 * 
+	 * @param scanner		A scanner object
+	 * @return String 	the user's input command 
+	 */
+	public static String readCommand(Scanner scanner) {
+		System.out.print("command:");
+		return scanner.nextLine();
+	}	
+	
+	/**
+	 * Validates the user input for a specific COMMAND_TYPE.
+	 * 
+	 * @param userInput		the user's input
+	 * @return COMMAND_TYPE 
+	 */	
+	public static COMMAND_TYPE parseCommandType(String userInput) {
+		String commandWord = InputParserUtils.getCommandWord(userInput);
+		COMMAND_TYPE commandType = InputParserUtils.getCommandType(commandWord);
+		assert commandType != null;
+		
+		int numOfWords = InputParserUtils.countWords(userInput);
+		boolean notEnoughArguments = numOfWords < 
+				InputParserUtils.getNumOfWordsNeeded(commandType);
+		
+		if (notEnoughArguments) { 
+			return COMMAND_TYPE.INCOMPLETE;
+		} else {
+			return commandType;
+		}
+	}
+
+	/**
+	 * Parses the user input for task descriptions.
+	 * 
+	 * @param userInput		the user's input
+	 * @return String 
+	 */	
+	public static String parseDescription(String userInput) {
+		Pattern p = Pattern.compile("(?:^|)'([^']*?)'(?:$|)", 
+				Pattern.MULTILINE);
+        Matcher m = p.matcher(userInput);
+        if (m.find()) {
+        	String description = m.group().substring(1, m.group().length() - 1);
+        	return validateEmptyDescription(description);
+        } else {
+            return null;
+        }
+	}
+
+	/**
+	 * Returns null for empty task descriptions.
+	 * 
+	 * @param String
+	 * @return String 
+	 */	
+	public static String validateEmptyDescription(String description) {
+		if(description.length() <= 0){
+			return null;
+		}
+		return description;
+	}
+	
+	//@author A0099314Y
+	/**
+	 * Attempts to get the content between first and second space, 
+	 * and parse as integer. -1 for unsuccessful parsing.
+	 * 
+	 * @param userInput 
+	 * @return int
+	 */	
+	public static int parseId(String userInput) {
+	    String[] spaceDelimitedInput = userInput.split("\\s+");
+	    if (spaceDelimitedInput.length < 2) {
+	        return NOT_FOUND;
+	    }
+	    String firstArgument = spaceDelimitedInput[1];
+	    try {
+	        int id = Integer.parseInt(firstArgument);
+	        return id;
+	    } catch (NumberFormatException e) {
+	        return NOT_FOUND;
+	    }
+	}
+	
+	/**
+     * Attempts to get the content between second and third space, 
+     * and parse as milliseconds. -1 for unsuccessful parsing.
+     * 
+     * @param userInput 
+     * @return duration in milliseconds
+     */ 
+    public static long parseDuration(String userInput) {
+        String[] spaceDelimitedInput = userInput.split("\\s+");
+        if (spaceDelimitedInput.length < 3) {
+            return NOT_FOUND;
+        }
+        String secondArgument = spaceDelimitedInput[2];
+        long millis = InputParserUtils.parseDurationToMillis(secondArgument);
+        if (millis < 0) {
+            return NOT_FOUND;
+        } else {
+            return millis;
+        }
+    }
+    
+	/**
+	 * Strips the command and other irrelevant arguments in the user input.
+	 * @param userInput user input string
+	 * @param command the type of the command
+	 * @return the input string with irrelevant keywords stripped away
+	 */
+	public static String getDateTimeString(String userInput,
+	        COMMAND_TYPE command) {
+        String desc = parseDescription(userInput);
+        if (desc != null) {
+            userInput = userInput.replace(desc, "");
+        }
+        
+	    String[] inputWords = userInput.trim().split("\\s+");	    
+	    String[] relevantWord = 
+	            Arrays.copyOfRange(inputWords,
+	                               InputParserUtils.getNumWordsToTrim(command),
+	                               inputWords.length);
+	    return InputParserUtils.joinString(relevantWord, " ");
+	}    
+}
+

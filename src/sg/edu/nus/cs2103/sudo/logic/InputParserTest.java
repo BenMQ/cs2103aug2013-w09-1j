8d5f671af07f91508f53f2d1991f41e89ebc6c51
Yos Riady <yosriady@gmail.com>
2013-11-10 15:53:20 +0800

    Update InputParserTest

--- src/sg/edu/nus/cs2103/sudo/logic/InputParserTest.java
+++ src/sg/edu/nus/cs2103/sudo/logic/InputParserTest.java
@@ -37,22 +37,14 @@ public class InputParserTest {
     }	
 	
 	@Test
-	public void testParseStringDate(){
-		ArrayList<DateTime> dates = InputParser.parseDateTime("fake " +
-				"'fake' 2013-10-14T14:00 to 2013-10-14T16:00", 
-				COMMAND_TYPE.INVALID);
-		assertEquals(2, dates.size());		
-	}
-	
-	@Test
-	public void testParseOneDate() {
+	public void testParseEmptyDates() {
 		ArrayList<DateTime> dates = InputParser.parseDateTime("add " +
-				"'finish homework' by 29 december", COMMAND_TYPE.ADD);
-		assertEquals(1, dates.size());
+				"'finish homework'", COMMAND_TYPE.ADD);
+		assertEquals(0, dates.size());
 	}
 	
 	@Test
-	public void testParseTwoDates() {
+	public void testParseDates() {
 		ArrayList<DateTime> dates = InputParser.parseDateTime("add 'golf' " +
 				"from the day before next thursday to 19 december", 
 				COMMAND_TYPE.ADD);
@@ -73,78 +65,10 @@ public class InputParserTest {
 	}
 
 	@Test
-	public void testParseId(){
-		assertEquals(2, InputParser.parseId("edit 2 'make pancakes " +
-				"for breakfast'"));
-	}	
-	
-	@Test
-	public void testParseIdNotFound(){	
-		assertEquals(InputParser.NOT_FOUND, InputParser.parseId("add 'make waffles " +
-				"for breakfast'"));
-	}	
-	
-	@Test
-	public void testIncompleteCommand() throws IOException{
-		testCommand("add", Constants.MESSAGE_INCOMPLETE_COMMAND);
-	}
-	
-	@Test
-	public void testInvalidCommand() throws IOException{
-		testCommand("fishing", Constants.MESSAGE_INVALID_COMMAND);
-	}
-
-	@Test
-	public void testParseAddFloatingTask() throws IOException{
-		String userInput = "add 'make waffles for breakfast'";
-		String taskDescription = InputParser.parseDescription(userInput);
-		runCommand(userInput);
-		assertEquals(1, manager.getTasks().size());
-	}
-
-	@Test
-	public void testParseAddDeadlineTask() throws IOException{
-		String userInput = "add 'make waffles for breakfast' by Monday" +
-				" 14 October 2pm";
-		String taskDescription = InputParser.parseDescription(userInput);
-		ArrayList<DateTime> dateTimes = InputParser.parseDateTime(userInput, 
-				COMMAND_TYPE.ADD);
-		String endTime = dateTimes.get(0).toString(
-				"EEE dd MMMM hh:mm a");
-		String expectedOutput = String.format(Constants.MESSAGE_ADD_DEADLINE, 
-				taskDescription, endTime);
-		runCommand(userInput);
-		assertEquals("Mon Oct 14 14:00:00 SGT 2013", 
-				manager.getTasks().get(0).endTime.toDate().toString());
-		assertEquals(1, manager.getTasks().size());
-	}
-
-	@Test
-	public void testParseAddTimedTask() throws IOException{
-		String userInput = "add 'make waffles for breakfast' from 13 October" +
-				" to 14 October 2pm";
-		String taskDescription = InputParser.parseDescription(userInput);
-		ArrayList<DateTime> dateTimes = InputParser.parseDateTime(userInput, 
-				COMMAND_TYPE.ADD);
-		String startTime = dateTimes.get(0).toString(
-				"EEE dd MMMM hh:mm a");
-		String endTime = dateTimes.get(1).toString(
-				"EEE dd MMMM hh:mm a");
-		String expectedOutput = String.format(Constants.MESSAGE_ADD_TIMED, 
-				taskDescription, startTime, endTime);
-		runCommand(userInput);
-		
-		assertEquals("Sun Oct 13 14:00:00 SGT 2013", 
-				manager.getTasks().get(0).startTime.toDate().toString());
-		assertEquals("Mon Oct 14 14:00:00 SGT 2013", 
-				manager.getTasks().get(0).endTime.toDate().toString());
-		assertEquals(1, manager.getTasks().size());
-	}	
-	
-	@Test
 	public void testParseAddMissingDescription() throws IOException{
 		String userInput = "add nothing";
 		testCommand(userInput, Constants.MESSAGE_MISSING_DESCRIPTION + Constants.MESSAGE_EMPTY_LIST);
+		
 		assertEquals(0, manager.getTasks().size());
 	}
 	
@@ -152,7 +76,73 @@ public class InputParserTest {
 	public void testEmptyStringDescription() throws IOException{
 		String userInput = "add '' from 19 October 2013 to 22 October 2014";
 		runCommand(userInput);
+		
 		assertEquals(0, manager.getTasks().size());
+	}		
+	
+	@Test
+	public void testParseId(){
+		int actual = InputParser.parseId("edit 2 'make pancakes " +
+				"for breakfast'");
+		assertEquals(2, actual);
+	}
+	
+	@Test
+	public void testParseIdNotFound(){	
+		int actual = InputParser.parseId("add 'make waffles " +
+				"for breakfast'");
+		assertEquals(InputParser.NOT_FOUND, actual);
+	}
+	
+	@Test
+	public void testIncompleteCommands() throws IOException{
+		testCommand("add", Constants.MESSAGE_INCOMPLETE_COMMAND);
+		testCommand("search", Constants.MESSAGE_INCOMPLETE_COMMAND);
+		testCommand("edit 1", Constants.MESSAGE_INCOMPLETE_COMMAND);
+		testCommand("schedule 1", Constants.MESSAGE_INCOMPLETE_COMMAND);
+	}
+	
+	@Test
+	public void testInvalidCommands() throws IOException{
+		testCommand("fishing", Constants.MESSAGE_INVALID_COMMAND);
+	}
+
+	@Test
+	public void testParseAddFloatingTask() throws IOException{
+		String userInput = "add 'make waffles for breakfast'";
+		runCommand(userInput);
+		
+		assertEquals(1, manager.getTasks().size());
+		assertTrue(manager.getTasks().get(0).isFloatingTask());
+	}
+
+	@Test
+	public void testParseAddDeadlineTask() throws IOException{
+		String userInput = "add 'make waffles for breakfast' by Monday" +
+				" 14 October 2pm";
+		String expectedEndDate = "Mon Oct 14 14:00:00 SGT 2013";
+		runCommand(userInput);
+		
+		assertEquals(expectedEndDate, 
+				manager.getTasks().get(0).endTime.toDate().toString());
+		assertEquals(1, manager.getTasks().size());
+		assertTrue(manager.getTasks().get(0).isDeadlineTask());
+	}
+
+	@Test
+	public void testParseAddTimedTask() throws IOException{
+		String userInput = "add 'make waffles for breakfast' from 13 October" +
+				" to 14 October 2pm";
+		String expectedStartDate = "Sun Oct 13 14:00:00 SGT 2013";
+		String expectedEndDate = "Mon Oct 14 14:00:00 SGT 2013";
+		runCommand(userInput);
+		
+		assertEquals(expectedStartDate, 
+				manager.getTasks().get(0).startTime.toDate().toString());
+		assertEquals(expectedEndDate, 
+				manager.getTasks().get(0).endTime.toDate().toString());
+		assertEquals(1, manager.getTasks().size());
+		assertTrue(manager.getTasks().get(0).isTimedTask());
 	}	
 	
 	@Test
@@ -160,6 +150,7 @@ public class InputParserTest {
 		String userInput = "add 'time travel' from 19 October 2013 to 22 " +
 				"October 2014 to 24 November 2016";
 		runCommand(userInput);
+		
 		assertEquals(0, manager.getTasks().size());
 	}
 	
